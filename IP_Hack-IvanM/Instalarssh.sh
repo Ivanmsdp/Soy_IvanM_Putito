91170a89f291f8abd96f1aa344256f5b2f20b88b
Ivanmsdp <lookzii525@gmail.com>
2025-10-25 21:14:02 -0600

    Update Instalarssh.sh to install bs4 and python3
    Removed old installation commands and added new package installations.

--- IP_Hack-IvanM/Instalarssh.sh
+++ IP_Hack-IvanM/Instalarssh.sh
@@ -1,19 +1,3 @@
-
-echo '[!] Updating...'
-apt-get update > install.log
-echo
-echo '[!] Installing Dependencies...'
-echo '    Python3'
-apt-get -y install python3 python3-pip &>> install.log
-echo '    PHP'
-apt-get -y install php &>> install.log
-echo '    ssh'
-apt-get -y install ssh &>> install.log
-echo '    Requests'
-pip3 install requests &>> install.log
-echo
-echo '[!] Setting Permissions...'
-chmod 777 template/nearyou/php/info.txt
-chmod 777 template/nearyou/php/result.txt
-echo
-echo '[!] Installed.'
+apt-get install python3
+pip3 install bs4
+pip3 install requests
